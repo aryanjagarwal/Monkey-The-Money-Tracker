52734ef3a3f71b42743997b8578ebec6dac5a813
Aryan Jagarwal <ajbaggar@gmail.com>
2022-07-17 19:53:25 +0530

    Initial Commit

---
+++ Monkey-The Money Tracker/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  Monkey-The Money Tracker
+//
+//  Created by Aryan Jagarwal on 17/07/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+}
