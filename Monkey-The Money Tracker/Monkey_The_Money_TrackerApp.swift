4c04a0b3e122bd5e28faa29feaf7f9f99f213551
Aryan Jagarwal <ajbaggar@gmail.com>
2022-07-17 18:34:19 +0530

    Initial Commit

---
+++ Monkey-The Money Tracker/Monkey_The_Money_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monkey_The_Money_TrackerApp.swift
+//  Monkey-The Money Tracker
+//
+//  Created by Aryan Jagarwal on 17/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Monkey_The_Money_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
